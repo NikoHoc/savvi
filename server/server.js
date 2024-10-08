e85058357135d3c2a72b4037e7bc8b0cd85154e2
FlyingLemonade <christopher.ryan7231@gmail.com>
2024-10-08 21:36:37 +0700

    Skibidi Toilet

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+// Ini buat load file public e
+app.use(express.static("../public"));
+
+app.get("/", (req, res) => {
+  res.redirect("dashboard.html");
+});
+
+app.listen(port, () => {
+  console.log(`Web is running on port ${port}`);
+});
